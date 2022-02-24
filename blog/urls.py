2c34b7aa534d0b790e82d3039c61f83adefb36cc
sameer <maharjansameer111@gmail.com>
2022-02-24 22:17:06 +0545

    templates part 4

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from blog.views import home , about
+
+app_name = 'blog'
 urlpatterns = [
     path('',home, name='blog-home'),
     path('about/',about, name='blog-about'),
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+    { 
+        'author':'sameer',
+        'title' : 'blog 1',
+        'content': 'first post',
+        'date_posted' : 'aug 4 2342'
+    },
+    { 
+        'author':'reemas',
+        'title' : 'blog 2',
+        'content': 'second post',
+        'date_posted' : 'aug 5 2342'
+    }
+]
+
 def home(request):
-    return HttpResponse('Blog home')
+    context = {
+        'posts' : posts,
+        'title':'Home',
+
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('Blog about')
+    context = {
+        'title':'About',
+    }
+    return render(request, 'blog/about.html', context)
