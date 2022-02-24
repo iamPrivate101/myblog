0dfce5604fa623fd98eb993e20bfb18be80a5007
sameer <maharjansameer111@gmail.com>
2022-02-24 22:40:23 +0545

    post database create part 5

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-posts = [
-    { 
-        'author':'sameer',
-        'title' : 'blog 1',
-        'content': 'first post',
-        'date_posted' : 'aug 4 2342'
-    },
-    { 
-        'author':'reemas',
-        'title' : 'blog 2',
-        'content': 'second post',
-        'date_posted' : 'aug 5 2342'
-    }
-]
+from blog.models import Post
+
 
 def home(request):
     context = {
-        'posts' : posts,
-        'title':'Home',
-
+        "posts": Post.objects.all(),
+        "title": "Home",
     }
-    return render(request, 'blog/home.html',context)
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
     context = {
-        'title':'About',
+        "title": "About",
     }
-    return render(request, 'blog/about.html', context)
+    return render(request, "blog/about.html", context)
