698a9be14bc6283fba61f77f11304b0946fcec51
sameer <maharjansameer111@gmail.com>
2022-02-26 13:21:55 +0545

    login and logout part 7

--- config/settings.py
+++ config/settings.py
@@ -130,4 +130,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'blog:blog-home'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/views.py
+++ users/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}')
-            return redirect('blog:blog-home')
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Your account has been created! You are now able to login "
+            )
+            return redirect("login***")
     else:
         form = UserRegisterForm()
 
     context = {
-        'form' : form,
-        'title': 'Register',
+        "form": form,
+        "title": "Register",
     }
-    return render(request,'users/register.html', context)
+    return render(request, "users/register.html", context)
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
